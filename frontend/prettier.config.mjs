d2f9dcb339177c35f70f2408dbe5b767b3f6eda6
koxrtx <koxrtx2003@yahoo.co.jp>
2026-08-03 00:12:45 +0900

    Prettierの導入

---
+++ frontend/prettier.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  singleQuote: true, // 文字列はシングルクオートを使用
+  semi: true, // 行末にセミコロンあり
+  tabWidth: 2, // インデントはスペース2つ
+  bracketSpacing: true, // オブジェクトの波括弧の間にスペースを入れる
+};
+
+export default config;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Nunito, Zen_Maru_Gothic } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Nunito, Zen_Maru_Gothic } from 'next/font/google';
+import './globals.css';
 
 const nunito = Nunito({
-  variable: "--font-nunito",
-  subsets: ["latin"],
+  variable: '--font-nunito',
+  subsets: ['latin'],
 });
 
 const zenMaru = Zen_Maru_Gothic({
-  variable:"--font-zen-maru",
-  weight: ["400", "700"],
+  variable: '--font-zen-maru',
+  weight: ['400', '700'],
 });
 
 export const metadata: Metadata = {
-  title: "Schedule Snap",
-  description: "AIで予定表をGoogleカレンダーに登録するアプリ",
+  title: 'Schedule Snap',
+  description: 'AIで予定表をGoogleカレンダーに登録するアプリ',
 };
 
 export default function RootLayout({
