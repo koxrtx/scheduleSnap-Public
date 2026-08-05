0f1039b912e70ce42af830f74d5138b50d7d6de5
koxrtx <koxrtx2003@yahoo.co.jp>
2026-08-05 15:05:20 +0900

    CORS設定

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,13 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 // 認証ルート
 import authRoute from "./routes/auth.js";
-
+// CORS
+import corsMiddleware from "./middleware/cors.js";
 
 const app = new Hono()
+app.use('/api/*', corsMiddleware())
 app.route("/", authRoute)
+
 
 // Hono　が動作してるか確認用ルートのためコメントアウト（削除してもいい）
 // app.get('/', (c) => {
--- frontend/src/components/AuthProvider.tsx
+++ frontend/src/components/AuthProvider.tsx
@@ -2,7 +2,17 @@
 "use client"
 
 // ログイン情報を子コンポーネントに渡す機能
-import { SessionProvider } from "@hono/auth-js/react"
+import {
+  SessionProvider,
+  authConfigManager,
+} from "@hono/auth-js/react"
+
+// Auth.jsの認証APIの接続先をHono側のURLに設定する
+// 公式に記載あり
+authConfigManager.setConfig({
+  baseUrl: "http://localhost:3001",
+  basePath: "/api/v1/auth",
+})
 
 export default function AuthProvider({
   children,
